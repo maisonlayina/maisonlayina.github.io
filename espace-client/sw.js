0e53949d41505066be4bbf65db9fa8f55bd6c19b
Layina - Maison de la tranformation <melissafkh@gmail.com>
2026-09-15 02:45:51 +0200

    Actualise le cache de l’espace client LAYINA

--- espace-client/sw.js
+++ espace-client/sw.js
@@ -1,4 +1,4 @@
-const CACHE='layina-espace-client-v2';
+const CACHE='layina-espace-client-v3';
 const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','../assets/logo-layina.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
